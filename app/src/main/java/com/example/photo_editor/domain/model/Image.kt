35c4f7a1e62924e895ca0c6b5fa1757e37ffdf61
C32-2 <vasilybichutsky@gmail.com>
2025-08-02 12:50:50 +0300

    First commit

---
+++ app/src/main/java/com/example/photo_editor/domain/model/Image.kt
@@ -0,0 +1,13 @@
+package com.example.photo_editor.domain.model
+
+import android.graphics.Bitmap
+
+data class ImageParameters(
+    var brightness: Float = 0.0f,
+    var contrast: Float = 0.0f
+)
+
+data class Image(
+    val originalBitmap: Bitmap,
+    var imageParameters: ImageParameters
+)
